1efb93433d0a911c594bf92d1fd85f4683dfce11
Shivam Phutela <shvmphutela05@gmail.com>
2024-08-25 15:51:27 +0530

    Create trie.py

---
+++ trie.py
@@ -0,0 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+    def delete(self, word):
+        def _delete(node, word, index):
+            if index == len(word):
+                if not node.is_end_of_word:
+                    return False
+                node.is_end_of_word = False
+                return len(node.children) == 0
+            char = word[index]
+            if char not in node.children:
+                return False
+            should_delete = _delete(node.children[char], word, index + 1)
+            if should_delete:
+                del node.children[char]
+                return not node.is_end_of_word and len(node.children) == 0
+            return False
+
+        _delete(self.root, word, 0)

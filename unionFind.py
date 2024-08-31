6b15c365d2a8949055f07e9e0c9eaf811f4c5eb7
Shivam Phutela <shvmphutela05@gmail.com>
2024-08-31 22:03:46 +0530

    Update unionFind.py

--- unionFind.py
+++ unionFind.py
@@ -1,15 +1,10 @@
-
-        
-        
 class UnionFind:
-    
     def __init__(self, n):
         self.parent = {}
         self.rank = {}
         self.islands = 0
         
     def find(self, n):
-        
         if n not in self.parent:
             self.parent[n] = n
             self.rank[n] = 0
@@ -26,40 +21,40 @@ class UnionFind:
             return
         self.islands -= 1
         
-        if(self.rank[n1] > self.rank[n2]):
-            self.parent[n2] = n1
-        elif(self.rank[n2] > self.rank[n1]):
-            self.parent[n1] = n2
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.parent[p1] = p2
         else:
-            self.parent[n2] = n1
-            self.rank[n1] += 1
+            self.parent[p2] = p1
+            self.rank[p1] += 1
             
 class Solution:
     def __init__(self):
         self.graph = set()
+        self.visited = set()  # Add visited set to track processed lands
         
-    def addLand(self,x,y):
-        self.graph.add((x,y))
+    def addLand(self, x, y):
+        self.graph.add((x, y))
         
     def numIslands2(self):
         uf = UnionFind(1000)
         n = 1000
-        res = []
         for r, c in self.graph:
-            
+            if (r, c) in self.visited:  # Skip if already visited
+                continue
             uf.find(r * n + c)
-            self.graph.add((r, c))
+            self.visited.add((r, c))  # Mark as visited
             for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
                 if (nr, nc) in self.graph:
                     uf.union(r * n + c, nr * n + nc)
-            
         return uf.islands
         
 g = Solution()
 
-g.addLand(1,1)
-print(g.numIslands2())
-g.addLand(1,2)
-print(g.numIslands2())
-g.addLand(2,4)
-print(g.numIslands2())
+g.addLand(1, 1)
+print(g.numIslands2())  # Output: 1
+g.addLand(1, 2)
+print(g.numIslands2())  # Output: 1
+g.addLand(2, 4)
+print(g.numIslands2())  # Output: 2
